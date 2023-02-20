4f7c784ea65e61cc7594ec20fa1ab0fd61f8e40f
Issou83 <issou8@gmail.com>
2023-02-20 17:13:02 +0100

    Création de base des 2 pages du site 'Home' et 'à propos' + création de la navbar evec ces deux onglets de navigation + mise en place du style à l'aide de sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div>
-      <h1>hello react</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/pages/About.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+        <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+        reprehenderit accusamus adipisci voluptatem rerum quod saepe hic quae
+        delectus in dolorem, similique molestias. Excepturi debitis itaque
+        fugiat hic quasi reprehenderit tempora magni exercitationem dolores
+        aspernatur repudiandae qui nihil, dignissimos amet quam quis facilis
+        possimus harum voluptatum eum. Blanditiis distinctio nam soluta maxime
+        eveniet aliquid unde deleniti quidem. Labore ex modi cupiditate nemo
+        natus hic sint reiciendis dicta adipisci quaerat libero maxime,
+        assumenda quisquam reprehenderit impedit, autem facere harum nesciunt
+        deleniti neque velit sequi earum. Laudantium facere quasi et sapiente
+        vitae inventore doloribus porro corporis voluptatem voluptas,
+        consequatur beatae amet voluptates!
+        <br />
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error modi
+          accusamus nihil explicabo. Facilis reprehenderit dolores excepturi
+          consequatur tempore amet saepe officiis, magni autem officia earum
+          omnis placeat voluptatem mollitia dolor quaerat totam, et accusantium
+          ut! Qui amet laborum temporibus aliquam? Odit in deleniti iure quo
+          minus, ullam repudiandae incidunt voluptatem unde magni obcaecati
+          accusantium. Nulla aliquid praesentium, officiis consequuntur
+          reiciendis libero vitae, ducimus optio consequatur mollitia quo quia
+          dolor maiores quis sed aspernatur deserunt cupiditate unde voluptatem!
+          Beatae molestiae voluptatibus magni voluptate, amet voluptatum facere
+          reprehenderit! Sunt eligendi ad dolores, dicta nisi officia corporis
+          aliquam nostrum sequi quibusdam corrupti unde reiciendis repellendus
+          minima commodi? Fugiat iure distinctio voluptates alias ipsam? Enim in
+          laborum ex delectus commodi. Laboriosam sequi voluptates quos sint
+          aliquid earum. Est in qui aut deserunt, quae recusandae possimus quas
+          labore harum voluptatem officia voluptate nam aspernatur ea
+          consequuntur ullam sit molestiae saepe odit nostrum. Mollitia impedit
+          harum et fugiat neque, eius alias voluptate quam cumque omnis ratione
+          quis enim corrupti assumenda a facilis tempora inventore natus ducimus
+          sunt asperiores commodi rerum voluptatum vero? Velit quod quo at,
+          provident numquam eveniet vel voluptas fugiat, et quisquam debitis
+          quia dolorum illo quasi nihil ducimus? Quisquam neque hic ipsa?
+        </p>
+      </p>
+    </div>
+  );
+};
+
+export default About;
