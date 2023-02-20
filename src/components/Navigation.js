a4e33109d4c6fbb4fa1dba824046b755e1891195
Issou83 <issou8@gmail.com>
2023-02-20 21:17:59 +0100

    Récupération des éléments de l'API grace à Axios + affiche de toutes les information + mise en place de toutes les rodios + ajout du boutton de réinitialisation des radios de selection continent

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
-    return (
-    <div className='navigation'>
-        <ul>
-            <NavLink to="/" className={(nav => (nav.isActive ? "nav-active" : null))}>
-                <li>acceuil</li>
-            </NavLink>
-            <NavLink to="/about" className={(nav => (nav.isActive ? "nav-active" : null))}>
-                <li>à propos</li>
-            </NavLink>
-        </ul>
+  return (
+    <div className="navigation">
+      <ul>
+        <li>
+        <NavLink
+          to="/"
+          className={(nav) => (nav.isActive ? "nav-active" : null)}>
+            acceuil
+        </NavLink>
+        </li>
+        <li>
+        <NavLink
+          to="/about"
+          className={(nav) => (nav.isActive ? "nav-active" : null)}
+        >
+          à propos
+        </NavLink>
+        </li>
+      </ul>
     </div>
-    );
+  );
 };
 
 export default Navigation;
